23ec98093637e2814bd6b021e0023abf5d65182a
sanghyeok-kim <slsnsi1212@gmail.com>
2023-04-07 21:33:52 +0900

    init: 프로젝트 파일 생성 및 초기 설정

---
+++ Habbit/Habbit/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  Habbit
+//
+//  Created by 김상혁 on 2023/04/07.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ Habbit/Habbit/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  Habbit
+//
+//  Created by 김상혁 on 2023/04/07.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let initialViewController = MainViewController()
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = initialViewController
+        window?.makeKeyAndVisible()
+    }
+}
+
